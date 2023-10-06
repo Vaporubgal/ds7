aa8464b14341329cf75ede34063ed36ee15fda43
Vaporubgal <108641265+Vaporubgal@users.noreply.github.com>
2023-10-05 22:41:05 -0500

    Add files via upload

---
+++ lab9/class/noticias.php
@@ -0,0 +1,44 @@
+<?php
+require_once('modelo.php');
+
+class noticia extends modeloCredencialesBD {
+    protected $titulo;
+    protected $texto;
+    protected $categoria;
+    protected $fecha;
+    protected $imagen;
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function consultar_noticias() {
+        $instruccion = "CALL sp_listar_noticias()";
+        $consulta = $this->_db->query($instruccion);
+        $resultado = $consulta->fetch_all(MYSQLI_ASSOC);
+
+        if (!$resultado) {
+            echo "Fallo al consultar las noticias";
+        } else {
+            $consulta->close();
+            $this->_db->close();
+            return $resultado;
+        }
+    }
+
+        public function consultar_noticias_filtro ($campo, $valor) {
+    $instruccion = "CALL sp_listar_noticias_filtro (' ".$campo."','".$valor."')";
+
+    $consulta=$this->_db->query($instruccion);
+    $resultado=$consulta->fetch_all (MYSQLI_ASSOC);
+        if ($resultado) {
+        return $resultado; 
+        $resultado->close(); 
+        $this->_db->close();
+        }
+    }
+}
+
+
+
+?>
