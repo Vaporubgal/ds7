4451b097f01c1f55ec40ac9f4781258686323fcc
Vaporubgal <108641265+Vaporubgal@users.noreply.github.com>
2023-10-26 19:52:24 -0500

    Add files via upload

---
+++ lab14/lab143.php
@@ -0,0 +1,86 @@
+<?php
+    session_start();
+?>
+<HTML LANG="es">
+<HEAD>
+    <TITLE>Laboratorio 11.1</TITLE>
+    <LINK REL="stylesheet" TYPE="text/css" HREF="css/estilo.css">
+</HEAD>
+<BODY>
+<H1>Consulta de noticias</H1>
+<?PHP
+if (isset($_SESSION["usuario_valido"]))
+{
+<?php
+require_once("class/noticias.php");
+
+$obj_noticia = new noticia();
+
+// Paginación
+$noticiasPorPagina = 5;
+$pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+$inicio = ($pagina - 1) * $noticiasPorPagina;
+
+// Utiliza la función consultarNoticiasPaginadas para obtener noticias y el total
+$resultados = $obj_noticia->consultarNoticiasPaginadas($inicio, $noticiasPorPagina);
+$noticias = $resultados['noticias'];
+$totalNoticias = $resultados['total'];
+
+if ($totalNoticias > 0) {
+    $totalPaginas = ceil($totalNoticias / $noticiasPorPagina);
+    $inicioNoticia = $inicio + 1;
+    $finNoticia = min($inicio + $noticiasPorPagina, $totalNoticias);
+    
+    echo "Mostrando noticias de $inicioNoticia a $finNoticia de un total de $totalNoticias. ";
+    
+    if ($pagina > 1) {
+        $paginaAnterior = $pagina - 1;
+        echo "<a href='?pagina=$paginaAnterior'>Anterior</a> | ";
+    }
+    
+    if ($pagina < $totalPaginas) {
+        $paginaSiguiente = $pagina + 1;
+        echo "<a href='?pagina=$paginaSiguiente'>Siguiente</a>";
+    }
+    
+    echo "<br>";
+
+    print ("<TABLE>\n");
+    print ("<TR>\n");
+    print ("<TH>Titulo</TH>\n");
+    print ("<TH>Texto</TH>\n");
+    print ("<TH>Categoria</TH>\n");
+    print ("<TH>Fecha</TH>\n");
+    print ("<TH>Imagen</TH>\n");
+    print ("</TR>\n");
+
+    foreach ($noticias as $resultado) {
+        print ("<TR>\n");
+        print ("<TD>" . $resultado['titulo'] . "</TD>\n");
+        print ("<TD>" . $resultado['texto'] . "</TD>\n");
+        print ("<TD>" . $resultado['categoria'] . "</TD>\n");
+        print ("<TD>" . date("j/n/Y", strtotime($resultado['fecha'])) . "</TD>\n");
+
+        if ($resultado['imagen'] != "") {
+            print ("<TD><A TARGET='_blank' HREF='img/" . $resultado['imagen'] . "'><IMG BORDER='0' SRC='img/iconotexto.gif'></A></TD>\n");
+        } else {
+            print ("<TD>&nbsp;</TD>\n");
+        }
+        print ("</TR>\n");
+    }
+    print ("</TABLE>\n");
+} else {
+    print ("No hay noticias disponibles");
+}
+<P>[ <A HREF='login.php'>Menu principal</A> ]</P>
+}
+else
+{
+    print ("<BR><BR>\n");
+    print ("<P ALIGN='CENTER'>Acceso no autorizado</P>\n");
+    print ("<P ALIGN='CENTER'>[ <A HREF='login.php' TARGET='_top'>Conectar</A> ]</P>\n"); 
+}
+
+?>
+</BODY>
+</HTML>
